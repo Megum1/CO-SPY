e00718d7baeb1b301d596e0e33aaa50edf4d604b
Siyuan Cheng <46131617+Megum1@users.noreply.github.com>
2025-09-07 23:14:33 -0400

    Add files via upload

---
+++ ProGANDetectors/__init__.py
@@ -0,0 +1,5 @@
+from .artifact_detector import ArtifactDetectorProGAN
+from .semantic_detector import SemanticDetectorProGAN
+from .cospy_calibrate_detector import CospyCalibrateDetectorProGAN
+
+__all__ = ["ArtifactDetectorProGAN", "SemanticDetectorProGAN", "CospyCalibrateDetectorProGAN"]
